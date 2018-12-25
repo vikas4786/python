a7bda1575016657a19158b3aac30abe8cbe84adc
vikas <vikas4786@gmail.com>
2018-12-25 14:18:26 +0000

    changing permission

--- awsautomation.py
+++ awsautomation.py
@@ -1,16 +1,35 @@
 import boto3
 import json
+import logging
+import time
+logging.basicConfig(level="INFO",format='%(asctime)s:%(message)s')
 
 class aws_vpc:
         def __init__(self):
             self.ec2=boto3.resource("ec2")
             fp=open("network.json")
-            self.v_data=json.load(fp)
+	    try:
+       	        self.v_data=json.load(fp)
+		logging.info("network.json file is in good state")
+	    except ValueError as v_error:
+		logging.error("{} is the error in network.json file".format(v_error))
             fp.close()
                 
         def vpc_create(self):
             v_vpcname=self.v_data["aws_vpc"]["vpc"]["vpcname"]
             v_vpccidr=self.v_data["aws_vpc"]["vpc"]["vpccidr"]
+            logging.info("checking if vpc already exists")
+	    vpc_obj=boto3.client("ec2")
+            v_vpc_info=vpc_obj.describe_vpcs()
+	    v_flag=False
+	    for v_vpcn in range(len(v_vpc_info["Vpcs"])):
+		cidr= v_vpc_info["Vpcs"][v_vpcn]["CidrBlock"]
+		if cidr == v_vpccidr:
+		   v_flag=True
+            if v_flag == True:
+		logging.error("{} address space overlapping ...".format(v_vpccidr))
+		time.sleep(5)
+		exit(123)
             self.v_vpc=self.ec2.create_vpc(CidrBlock=v_vpccidr)
             self.v_vpc.create_tags(Tags=[{"Key":"Name","Value":v_vpcname}])
             
@@ -20,6 +39,10 @@ class aws_vpc:
 		v_subnetcidr=self.v_data["aws_vpc"]["subnet"][v_sub]["sub_cidr"]
 		subnet = self.ec2.create_subnet(CidrBlock=v_subnetcidr, VpcId=self.v_vpc.id)
 		subnet.create_tags(Tags=[{"Key":"Name","Value":v_subnetname}])
+	def igw_create(self):
+	   	v_igw=self.v_data["aws_vpc"]["igw"]["Igwname"]
+		print v_igw 
 s=aws_vpc()
 s.vpc_create()
-s.subnet_create()
+#s.subnet_create()
+#s.igw_create()
--- awsvpcdelsearch.py
+++ awsvpcdelsearch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import boto3
 import json
 
@@ -7,11 +8,16 @@ class aws_vpcd():
 	    self.v_vpc_info=self.ec2.describe_vpcs()
 	    for v_vpc in range(len(self.v_vpc_info['Vpcs'])):
 		vpcid=self.v_vpc_info['Vpcs'][v_vpc]['VpcId']
+		vpcidtag=self.v_vpc_info['Vpcs'][v_vpc]['CidrBlock']
  		print vpcid
+ 		print vpcidtag
 	
 	def aws_vpcfile(self):
 	    fp=open("network.json")
 	    self.v_data=json.load(fp)
+	    cust_cidr=self.v_data["aws_vpc"]["vpc"]["vpccidr"]
+	    print( "{}.is customer cidr required to be deleted".format(cust_cidr))
+            fp.close()
   
 s=aws_vpcd()
 s.vpc()
